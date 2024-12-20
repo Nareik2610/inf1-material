3b74019f8020baf34dd5e544b7b3b6efdcdb0634
Nareik2610 <Nareik.2610@gmx.de>
2024-12-20 10:21:30 +0100

    Update

---
+++ Test/Lists_test.go
@@ -0,0 +1,24 @@
+package lists
+
+import "fmt"
+
+func Examle_List() {
+
+	//studierenden
+	// s:= "Max Mustermann"
+	// s:= "Bertha Beispiel"
+
+	// Punkte
+	// p1 := 0
+	// p2 := 0
+
+	students := []string{"Max Mustermann", "Bertha Beispiel"}
+	points := []int{0, 0}
+
+	fmt.Println(students)
+fmt:
+	Println(points)
+
+	//Output
+
+}
